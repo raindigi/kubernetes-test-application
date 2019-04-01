cfcec455978d34cad2a75f337ba5ab570109c6ff
Daniel Weibel <danielmweibel@gmail.com>
2019-04-01 20:58:28 +0200

    Pass DB hostname as env variable to webserver container

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 
 const port = 8080;
-const dbHost = 'db-service';
+const dbHost = process.env.DB_HOST;
 const dbUri = `mongodb://${dbHost}:27017`;
 
 function handler (request, response) {
@@ -22,12 +22,14 @@ function handler (request, response) {
 http.createServer(handler).listen(port);
 console.log(`Listening on port ${port}...`);
 
+// Connect to database and get string to display
 async function getStringFromDb(fun) {
+  console.log(`Connecting to ${dbUri}...`);
   const dbClient = new MongoClient(dbUri, {useNewUrlParser: true});
   await dbClient.connect();
-  const col = dbClient.db('html').collection('html');
-  const docs = await col.find({index: {$exists: true}}).toArray();
+  const collection = dbClient.db('html').collection('html');
+  const docs = await collection.find({index: {$exists: true}}).toArray();
   dbClient.close();
-  if (docs.length < 1) throw new Error(`String not found in: ${docs}`);
+  if (docs.length < 1) throw new Error("String not found in database");
   return docs[0].index;
 }
