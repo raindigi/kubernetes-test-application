e120ab42eecf5f98e888d7b7fe58982e39c9ab0b
Daniel Weibel <danielmweibel@gmail.com>
2019-04-02 10:58:48 +0200

    Pass DB name and collection to webserver Pod in ConfigMap

--- webserver/app.js
+++ webserver/app.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 
-const port = 8080
+
+checkEnvVar(process.env.DB_HOST, 'DB_HOST');
+checkEnvVar(process.env.DB_NAME, 'DB_NAME');
+checkEnvVar(process.env.DB_COLLECTION, 'DB_COLLECTION');
 const dbHost = process.env.DB_HOST;
-if (!dbHost) throw new Error("Error: DB_HOST environment variable is not set");
+const dbName = process.env.DB_NAME; 
+const dbCollection = process.env.DB_COLLECTION; 
+
+const port = 8080
 const dbUri = `mongodb://${dbHost}:27017`;
 
 function handler (request, response) {
@@ -28,9 +34,14 @@ async function getStringFromDb(fun) {
   console.log(`Connecting to ${dbUri}...`);
   const dbClient = new MongoClient(dbUri, {useNewUrlParser: true});
   await dbClient.connect();
-  const collection = dbClient.db('html').collection('html');
+  const collection = dbClient.db(dbName).collection(dbCollection);
   const docs = await collection.find({index: {$exists: true}}).toArray();
   dbClient.close();
   if (docs.length < 1) throw new Error("String not found in database");
   return docs[0].index;
+}
+
+function checkEnvVar(value, name) {
+  if (!value)
+    throw new Error(`Error: ${name} environment variable is not set`);
 }
